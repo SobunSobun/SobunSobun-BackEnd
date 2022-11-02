a2ba966d40b8bb482ee835c898c28247bdab72d6
kitaee <dlrlxo999@naver.com>
2022-11-02 19:55:11 +0900

    ♻️ [REFACTOR] : AWS Server Setting

--- src/main/java/numble/sobunsobun/SobunsobunApplication.java
+++ src/main/java/numble/sobunsobun/SobunsobunApplication.java
@@ -8,7 +8,6 @@ public class SobunsobunApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SobunsobunApplication.class, args);
-		System.out.println("hello world!");
 	}
 
 }
