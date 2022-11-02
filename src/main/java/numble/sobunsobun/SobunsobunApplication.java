3268959dcea898bb0b46c6aef95ba451be70e25d
SL313 <rebin0313@gmail.com>
2022-11-02 13:56:33 +0900

    Initial Setting

---
+++ src/main/java/numble/sobunsobun/SobunsobunApplication.java
@@ -0,0 +1,13 @@
+package numble.sobunsobun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobunsobunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobunsobunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/numble/sobunsobun/SobunsobunApplicationTests.java
@@ -0,0 +1,13 @@
+package numble.sobunsobun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SobunsobunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
