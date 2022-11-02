dc1f4018dd61dae2411e7491ab0ce04e533ac129
kitaee <dlrlxo999@naver.com>
2022-11-02 19:10:12 +0900

    ♻️ [REFACTOR] : AWS Server Setting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,5 +30,3 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar -Dspring.config.location=/home/ubuntu/app/src/main/resources/application-mysql.properties -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
-
-echo "> Check"
