58ca18144d80b48ac246de3bd80ad30587d861cc
SL313 <rebin0313@gmail.com>
2022-11-09 19:53:23 +0900

    :sparkles: [FEAT] : Modifing Post API

--- src/main/java/numble/sobunsobun/post/PostController.java
+++ src/main/java/numble/sobunsobun/post/PostController.java
@@ -2,6 +2,7 @@ package numble.sobunsobun.post;
 
 import lombok.RequiredArgsConstructor;
 import numble.sobunsobun.post.domain.Post;
+import numble.sobunsobun.post.dto.ModifyPostDto;
 import numble.sobunsobun.post.dto.RegisterPostDto;
 import numble.sobunsobun.post.service.PostService;
 import numble.sobunsobun.user.domain.User;
@@ -13,7 +14,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/post")
@@ -69,6 +69,41 @@ public class PostController {
             return new ResponseEntity<>("게시글 삭제 완료", HttpStatus.OK);
         } else{
             return new ResponseEntity<>("작성자만 삭제 가능", HttpStatus.OK);
+        }
+    }
+
+    /**
+     * 게시글 수정 API
+     * */
+    @PatchMapping("/{postId}")
+    public ResponseEntity<String> modify(@PathVariable Long postId, ModifyPostDto modifyPostDto){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails) principal).getUsername();
+
+        UserDetails loginUser = userService.loadUserByUsername(username);
+        User user = (User) loginUser;
+
+        Post post = postService.getPostEntity(postId);
+
+        if (post.getUserId().equals(user.getUserId())){
+            if (post.getApplyNumber() > 1){
+                return new ResponseEntity<>("모집에 참여한 사람이 있어 수정이 불가합니다.", HttpStatus.OK);
+            } else{
+
+                post.setTitle(modifyPostDto.getTitle());
+                post.setContent(modifyPostDto.getContent());
+                post.setModifiedTime(LocalDateTime.now());
+                post.setRecruitmentNumber(modifyPostDto.getRecruitmentNumber());
+                post.setCategory(modifyPostDto.getCategory());
+                post.setMeetingTime(modifyPostDto.getMeetingTime());
+                post.setMarket(modifyPostDto.getMarket());
+
+                postService.savePost(post);
+
+                return new ResponseEntity<>("게시글 수정 완료", HttpStatus.OK);
+            }
+        } else{
+            return new ResponseEntity<>("작성자만 수정 가능", HttpStatus.OK);
         }
     }
 }
