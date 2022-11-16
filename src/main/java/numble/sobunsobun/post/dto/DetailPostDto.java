1f5df64c27d83e76436e0caf18b59ac38b1e9f2b
SL313 <rebin0313@gmail.com>
2022-11-16 11:37:39 +0900

    :memo: [ADD] : Detail Post Dto

---
+++ src/main/java/numble/sobunsobun/post/dto/DetailPostDto.java
@@ -0,0 +1,16 @@
+package numble.sobunsobun.post.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class DetailPostDto {
+    private String title;
+    private String content;
+    private Integer recruitmentNumber;
+    private Integer applyNumber;
+    private String category;
+    private String meetingTime;
+    private String market;
+    private String uploadTime;
+}
