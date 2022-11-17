41c778eae2be6567e1302db06a0f78a749e8a369
kitaee <dlrlxo999@naver.com>
2022-11-17 16:35:15 +0900

    🚑️ [HOTFIX] : CORS 세팅

--- src/main/java/numble/sobunsobun/security/WebMvcConfig.java
+++ src/main/java/numble/sobunsobun/security/WebMvcConfig.java
@@ -8,10 +8,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedHeaders("*")
-                .allowedOrigins("http://15.164.112.119:8080/")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+//                .allowedOriginPatterns("*")
+//                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
                 .maxAge(3000);
     }
 }
