b304ce2641e100575669e54c2071445352c790c6
kitaee <dlrlxo999@naver.com>
2022-11-17 16:42:59 +0900

    🚑️ [HOTFIX] : CORS 세팅

--- src/main/java/numble/sobunsobun/security/WebMvcConfig.java
+++ src/main/java/numble/sobunsobun/security/WebMvcConfig.java
@@ -1,16 +1,20 @@
 package numble.sobunsobun.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-//                .allowedOriginPatterns("*")
-//                .allowedHeaders("*")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
                 .maxAge(3000);
     }
