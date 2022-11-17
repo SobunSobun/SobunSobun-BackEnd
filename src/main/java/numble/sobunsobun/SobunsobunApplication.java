1d9333a0eb12fe3610f13f1031f17ee83a8377bc
이기태 <91439717+kitaee@users.noreply.github.com>
2022-11-17 15:31:33 +0900

    Update SobunsobunApplication.java

--- src/main/java/numble/sobunsobun/SobunsobunApplication.java
+++ src/main/java/numble/sobunsobun/SobunsobunApplication.java
@@ -3,7 +3,7 @@ package numble.sobunsobun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = ContextStackAutoConfiguration.class)
+@SpringBootApplication
 public class SobunsobunApplication {
 
 	public static void main(String[] args) {
