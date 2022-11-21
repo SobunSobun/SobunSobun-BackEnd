e45739237eefee0e315cc2480155c66fa397b771
SL313 <rebin0313@gmail.com>
2022-11-21 18:13:21 +0900

    :memo: [ADD] : change status method

--- src/main/java/numble/sobunsobun/myPage/service/MyPageService.java
+++ src/main/java/numble/sobunsobun/myPage/service/MyPageService.java
@@ -1,2 +1,22 @@
-package numble.sobunsobun.myPage.service;public class MyPageService {
+package numble.sobunsobun.myPage.service;
+
+import lombok.RequiredArgsConstructor;
+import numble.sobunsobun.user.domain.User;
+import numble.sobunsobun.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MyPageService {
+
+    private final UserRepository userRepository;
+
+    public void deleteUser(Long userId){
+        Optional<User> user = userRepository.findByUserIdAndStatus(userId, 1);
+        user.ifPresent(value -> value.setStatus(0));
+    }
 }
