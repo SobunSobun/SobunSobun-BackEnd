90af9b3352da061fa5c2049f00c2098d01a7ddc5
SL313 <rebin0313@gmail.com>
2022-11-24 19:01:31 +0900

    :hammer: [MOD] : change the object type

--- src/main/java/numble/sobunsobun/post/repository/PostRepository.java
+++ src/main/java/numble/sobunsobun/post/repository/PostRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAllByCategoryAndLocationAndStatusAndIsFullOrderByCreatedTimeDesc(String category, String location, Integer status, Boolean isFull, Pageable pageable);
     Page<Post> findAllByLocationAndStatusAndIsFullOrderByCreatedTimeDesc(String location, Integer status, Boolean isFull, Pageable pageable);
-    Page<Post> findAllByUserIdAndStatusOrderByCreatedTimeDesc(Long userId, Integer status, Pageable pageable);
+    List<Post> findAllByUserIdAndStatusOrderByCreatedTimeDesc(Long userId, Integer status);
 }
