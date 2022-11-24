21ff8fe0f7fc856d4b17b47d055ada1a2aad21a9
SL313 <rebin0313@gmail.com>
2022-11-24 21:06:02 +0900

    :memo: [ADD] : find apply object method

--- src/main/java/numble/sobunsobun/apply/repository/ApplyRepository.java
+++ src/main/java/numble/sobunsobun/apply/repository/ApplyRepository.java
@@ -4,9 +4,11 @@ import numble.sobunsobun.apply.domain.Apply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
-    Optional<Apply> findByPostIdAndUserId(Long postId, Long userID);
+    Optional<Apply> findByPostIdAndUserId(Long postId, Long userId);
+    List<Apply> findByUserIdOrderByApplyId(Long userId);
 }
